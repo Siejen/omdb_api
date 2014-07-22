66ba5d239d3c45f4d3754ae2984c720f3571797d
Siejen Yin-Stevenson <siejenyin@gmail.com>
2014-07-22 16:41:28 -0700

    starting point after lecture

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require("express");
+var request = require("request");
 
 var app = express();
 
 app.get('/', function(req, res){
   res.render('index.ejs');
+});
+
+app.get('/search', function(req, res){ 
+  var query = req.query.searchTerm;
+  var url = "http://www.omdbapi.com/?s=" + query;
+  request(url, function (error, response, body) {
+  	if (!error) {
+  		var data = JSON.parse(body);
+  		res.render("results.ejs", {movieList: data.Search});
+  	}
+  });
 });
 
 app.listen(3000);
